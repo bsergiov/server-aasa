d6bd3b778c5dca5a3a9bacc3d5da9eb84ff7a2cc
Sergio <bsergiovi@gmail.com>
2024-09-30 20:26:30 +0200

    feat: base settings project.

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Указываем базовый образ PHP
+FROM php:8.1-cli
+
+# Устанавливаем Composer (менеджер пакетов для PHP)
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+# Устанавливаем необходимые зависимости
+RUN apt-get update && apt-get install -y \
+    git \
+    unzip \
+    && apt-get clean
+
+# Устанавливаем директорию приложения
+WORKDIR /var/www/html
+
+# Копируем файлы проекта в контейнер
+COPY . .
+
+# Устанавливаем зависимости через Composer
+RUN composer install
+
+# Указываем порт, который слушает приложение (если планируете запускать встроенный PHP-сервер)
+EXPOSE 8000
+
+# Запускаем PHP встроенный сервер
+CMD ["php", "-S", "0.0.0.0:8000"]
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$app_id = $_ENV['APP_ID'];
+
+header('Content-Type: application/json');
+
+// Получение запрашиваемого URI
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($uri) {
+    case '/':
+        // Обработка корневого роута
+        echo json_encode(["message" => "Hello, this is the root route"]);
+        break;
+
+    case '/apple-app-site-association':
+        // Обработка apple-app-site-association
+        echo json_encode([
+            "applinks" => [
+                "apps" => [],
+                "details" => [
+                    [
+                        "appID" => $app_id,
+                        "paths" => ["*"]
+                    ]
+                ]
+            ]
+        ]);
+        break;
+
+    default:
+        // Обработка 404 для остальных запросов
+        http_response_code(404);
+        echo json_encode(["error" => "Route not found"]);
+        break;
+}
